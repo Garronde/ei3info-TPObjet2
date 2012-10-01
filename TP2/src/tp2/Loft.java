07d22e2ca3d4c23a86323cd23d9bef9f248b1d44
garronde <garrondeau@gmail.com>
2012-10-01 16:46:42 +0200

    Création incomplète de la classe Loft

---
+++ TP2/src/tp2/Loft.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2;
+
+import java.util.List;
+
+/**
+ *
+ * @author garrondeau
+ */
+public class Loft {
+    private Case[][] damier;
+    private int w;
+    private int h;
+    private List<Neuneu> participants;
+    
+    public Loft(int w, int h, int nbParticipants) {
+        /*ahhh je sais pas !!*/ nbParticipants = 10;
+        this.w = w;
+        this.h = h;
+    }
+
+    public afficheLoft() {
+
+    }
+    
+    public tourDeJeu(){
+        
+    }
+    
+    public addParticipant(){
+        
+    }
+}
