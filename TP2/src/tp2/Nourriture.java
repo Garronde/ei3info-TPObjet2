2eb242889b64257be401ee51b57fe2e30cd66132
garronde <garrondeau@gmail.com>
2012-10-01 16:49:43 +0200

    Création de la classe Nourriture

---
+++ TP2/src/tp2/Nourriture.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2;
+
+/**
+ *
+ * @author garrondeau
+ */
+public class Nourriture {
+
+    private int energie;
+
+    public Nourriture() {
+        energie = 55;
+    }
+}
