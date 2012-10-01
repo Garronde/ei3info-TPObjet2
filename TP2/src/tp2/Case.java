7c1d5a557daf1b9658a8a006078669aabda8f222
garronde <garrondeau@gmail.com>
2012-10-01 16:30:06 +0200

    Création de la classe Case

---
+++ TP2/src/tp2/Case.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tp2;
+
+import java.util.List;
+
+/**
+ *
+ * @author garrondeau
+ */
+public class Case {
+   private List<Object> contenuCase;
+}
