7c6fbd5ab3dba80fbc71acef532c9330d6a00c86
Tony Martin <tony.martin313@gmail.com>
2012-10-02 09:32:42 +0200

    Constructeur du Loft créé

--- TP2/src/tp2/Loft.java
+++ TP2/src/tp2/Loft.java
@@ -20,21 +20,42 @@ public class Loft {
         public Loft(int width, int height, int nbParticipants) {
                 this.w = width;
                 this.h = height;
-
+                //On remplit le damier avec de la nourriture
                 for (int i = 0; i < h; i++) {
                         for (int j = 0; j < w; j++) {
                                 damier[i][j] = new Case();
                                 if (Math.random() < 0.05) {
                                         damier[i][j].contenuCase.add(new Nourriture());
-                                } else if(Math.random() < 0.01) {
+                                } else if (Math.random() < 0.01) {
                                         damier[i][j].contenuCase.add(new Alcool());
                                 }
                         }
                 }
-                
+                //On place ensuite les participants sur le damier en vérifiant qu'ils soient sur une case vide
                 for (int i = 0; i < nbParticipants; i++) {
-                        
-                        participants.add(null);
+                        int random = (int) (Math.random() * 4) + 1;
+                        int x = (int) (Math.random() * (w));
+                        int y = (int) (Math.random() * (h));
+                        while (!damier[x][y].contenuCase.isEmpty()) {                                
+                                x = (int) (Math.random() * (w));
+                                y = (int) (Math.random() * (h));
+                        }
+                        Neuneu fils = null;
+                        switch (random) {
+                                case 1:
+                                        fils = new Vorace(x, y, this);
+                                        break;
+                                case 2:
+                                        fils = new Erratique(x, y, this);
+                                        break;
+                                case 3:
+                                        fils = new Cannibale(x, y, this);
+                                        break;
+                                case 4:
+                                        fils = new Lapin(x, y, this);
+                                        break;
+                        }
+                        participants.add(fils);
                 }
         }
 
