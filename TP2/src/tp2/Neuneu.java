b2448e03c3d136b5190c4c5fb599083535b70645
garronde <garrondeau@gmail.com>
2012-10-26 10:06:47 +0200

    Modification de la méthode reproduction de Neuneu (à compléter)

--- TP2/src/tp2/Neuneu.java
+++ TP2/src/tp2/Neuneu.java
@@ -10,11 +10,10 @@ package tp2;
  */
 public abstract class Neuneu {
 
-        protected int energie;
-        protected int x;
-        protected int y;
-        protected Loft loft;
-
+    protected int energie;
+    protected int x;
+    protected int y;
+    protected Loft loft;
 
     public int getEnergie() {
         return energie;
@@ -28,14 +27,12 @@ public abstract class Neuneu {
         return y;
     }
 
-
-        public Neuneu(int x, int y, Loft l) {
-                energie = 100;
-                this.x = x;
-                this.y = y;
-                loft = l;
-        }
-
+    public Neuneu(int x, int y, Loft l) {
+        energie = 100;
+        this.x = x;
+        this.y = y;
+        loft = l;
+    }
 
     public void bouger() {
     }
@@ -47,28 +44,36 @@ public abstract class Neuneu {
         }
     }
 
-        public Neuneu seReproduire(Neuneu partenaire) {
-                int random = (int) (Math.random() * 4) + 1;
-                Neuneu fils = null;
-                switch (random) {
-                        case 1:
-                                fils = new Vorace(x, y, loft);
-                                break;
-                        case 2:
-                                fils = new Erratique(x, y, loft);
-                                break;
-                        case 3:
-                                fils = new Cannibale(x, y, loft);
-                                break;
-                        case 4:
-                                fils = new Lapin(x, y, loft);
-                                break;
-                }
-                energie -= 45;
-                return fils;
-
+    public Neuneu seReproduire(Neuneu partenaire) {
+        if (this.energie > 45 && partenaire.energie > 45) /*S'il ont assez d'energie (>45) ils font un fils
+         * */ {
+            int random = (int) (Math.random() * 4) + 1;
+            Neuneu fils = null;
+            switch (random) {
+                case 1:
+                    fils = new Vorace(x, y, loft);
+                    break;
+                case 2:
+                    fils = new Erratique(x, y, loft);
+                    break;
+                case 3:
+                    fils = new Cannibale(x, y, loft);
+                    break;
+                case 4:
+                    fils = new Lapin(x, y, loft);
+                    break;
+            }
+            energie -= 45;
+            partenaire.energie -= 45;
+            return fils;
         }
-        energie -= 45;
-        return fils;
+        if (this.energie <= 45) {
+            /* tuer le neueneu
+             */
+        }
+        if (partenaire.energie <= 45) {
+            /* tuer le partenaire
+             */
+        }
     }
 }
