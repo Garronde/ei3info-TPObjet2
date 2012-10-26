7e60f1c13a1c1029a0cb8d50e1cb28082e62e8f5
garronde <garrondeau@gmail.com>
2012-10-26 10:09:35 +0200

    Ajout d'une méthode mangerNeuneu dans canibal

--- TP2/src/tp2/Cannibale.java
+++ TP2/src/tp2/Cannibale.java
@@ -4,15 +4,22 @@
  */
 package tp2;
 
-
 public class Cannibale extends Neuneu {
 
-        public Cannibale(int x, int y, Loft l) {
-                super(x, y, l);
+    public Cannibale(int x, int y, Loft l) {
+        super(x, y, l);
+    }
+
+    @Override
+    public void bouger() {
+    }
+
+    public void manger(Neuneu v) {
+        energie = v.energie + energie;
+        if (energie > 100) {
+            energie = 100;
+            /* il faut tuer le Neuneu victime v*/
         }
-        
-        @Override
-        public void bouger() {
-                
-        }
+
+    }
 }
