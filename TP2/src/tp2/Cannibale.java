1f5712cf0bc1e6b84dc6568cf363b7c26cd79b5f
garronde <garrondeau@gmail.com>
2012-10-26 11:30:49 +0200

    Methodes bouger de cannibale et vorace

--- TP2/src/tp2/Cannibale.java
+++ TP2/src/tp2/Cannibale.java
@@ -10,8 +10,28 @@ public class Cannibale extends Neuneu {
         super(x, y, l);
     }
 
-    @Override
+        @Override
     public void bouger() {
+
+        int a = 0, b = 0;
+        boolean trouve = false;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+
+                if (loft.damier[x + i][y + j].contenuCase.get(0) instanceof Neuneu) {
+                    a = i;
+                    b = j;
+                    trouve = true;
+                }
+
+            }
+        }
+        if (!trouve) {
+            super.bouger();
+        } else {
+            this.x += a;
+            this.y += b;
+        }
     }
 
     public void mangerLofteur(Neuneu v) {
--- TP2/src/tp2/Vorace.java
+++ TP2/src/tp2/Vorace.java
@@ -11,8 +11,27 @@ public class Vorace extends Neuneu {
                 super(x, y,l);
         }
         
-        @Override
-        public void bouger() {
-                
+            @Override
+    public void bouger() {
+
+        int a = 0, b = 0;
+        boolean trouve = false;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+
+                if (loft.damier[x + i][y + j].contenuCase.get(0) instanceof Nourriture) {
+                    a = i;
+                    b = j;
+                    trouve = true;
+                }
+
+            }
         }
+        if (!trouve) {
+            super.bouger();
+        } else {
+            this.x += a;
+            this.y += b;
+        }
+    }
 }
