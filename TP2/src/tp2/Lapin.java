77e33b69d1ec5e08ee4c0f5860b2017fa2b6be48
garronde <garrondeau@gmail.com>
2012-10-26 11:26:32 +0200

    commit pour synchro début bouger lapin

--- TP2/src/tp2/Lapin.java
+++ TP2/src/tp2/Lapin.java
@@ -4,15 +4,41 @@
  */
 package tp2;
 
-
 public class Lapin extends Neuneu {
 
-        public Lapin(int x, int y, Loft l) {
-                super(x, y, l);
+    public Lapin(int x, int y, Loft l) {
+        super(x, y, l);
+    }
+
+    @Override
+    public void bouger() {
+      
+            int a, b;
+            boolean trouve = false;
+            for (i = -1; i++; i <= 1) {
+                for (j = -1; j++; j <= 1) {
+                    if ((0 <= this.x + i) && (this.x + i <= loft.getW()) && (0 <= this.x + i) && (this.x + i <= 99)) {
+                        if (damier[x + i][y + j].contenuCase.get(0) instanceof Neuneu) {
+                            a = i;
+                            b = j;
+                            trouve = true;
+                        }
+                    }
+                }
+            }
+            if (!trouve) {
+                super.bouger();
+            } else {
+                this.x += a;
+                this.y += b;
+            }
         }
+
+
+    
+
+    
+}
+}
         
-        @Override
-        public void bouger() {
-                
-        }
 }
